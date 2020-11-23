eab43216858623035c3fb7ed21b978626bb16cf1
Pete Gadomski <pete.gadomski@gmail.com>
2020-11-23 09:08:50 -0700

    Update csv to include time

--- examples/csv.rs
+++ examples/csv.rs
@@ -26,15 +26,26 @@ fn main() -> Result<(), Error> {
                 .long("decimate")
                 .short("d"),
         )
+        .arg(
+            Arg::with_name("include-time")
+                .help("include the time")
+                .long("include-time")
+                .short("t"),
+        )
         .get_matches();
     let input = matches.value_of("INPUT").unwrap();
     let reader = Reader::from_path(input)?;
     let step_by = matches.value_of("decimate").unwrap().parse().unwrap();
+    let include_time = matches.is_present("include-time");
     let mut output = File::create(matches.value_of("OUTPUT").unwrap()).unwrap();
-    writeln!(output, "latitude,longitude,altitude").unwrap();
+    write!(output, "latitude,longitude,altitude").unwrap();
+    if include_time {
+        write!(output, ",time").unwrap();
+    }
+    writeln!(output, "").unwrap();
     for result in reader.step_by(step_by) {
         let point = result?;
-        writeln!(
+        write!(
             output,
             "{},{},{}",
             point.latitude.to_degrees(),
@@ -42,6 +53,10 @@ fn main() -> Result<(), Error> {
             point.altitude,
         )
         .unwrap();
+        if include_time {
+            write!(output, "{}", point.time).unwrap();
+        }
+        writeln!(output, "").unwrap();
     }
     Ok(())
 }
